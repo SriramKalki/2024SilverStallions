2fe21547546220eb6118d310672147df83fdc5ad
robotics <ftc6547@gmail.com>
2023-11-17 12:39:51 -0600

    Added BlueNearAuton untested

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueNearAuton.java
@@ -0,0 +1,110 @@
+package org.firstinspires.ftc.teamcode;
+
+import static org.firstinspires.ftc.teamcode.vision.NewBluePropProcessor.Location.MIDDLE;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+import org.firstinspires.ftc.teamcode.vision.NewBluePropProcessor;
+import org.firstinspires.ftc.vision.VisionPortal;
+
+@Autonomous(name="Blue Near Auton")
+public class BlueNearAuton extends LinearOpMode {
+    private NewBluePropProcessor.Location location = MIDDLE;
+    private NewBluePropProcessor bluePropProcessor;
+    private VisionPortal visionPortal;
+
+    private DcMotor slide;
+    private DcMotor climb;
+    private DcMotor plane;
+    private Servo latch, boxWrist, intakeWrist;
+    private CRServo intake, belt; // belt is orange pass through thing
+    SampleMecanumDrive drive;
+
+    @Override
+    public void runOpMode(){
+        drive = new SampleMecanumDrive(hardwareMap);
+        bluePropProcessor = new NewBluePropProcessor(telemetry);
+        visionPortal = VisionPortal.easyCreateWithDefaults(
+                hardwareMap.get(WebcamName.class, "Webcam 1"), bluePropProcessor);
+
+        initHardware();
+
+        TrajectorySequence leftPurple = drive.trajectorySequenceBuilder(new Pose2d(0,0,Math.toRadians(0)))
+                .addTemporalMarker(() -> {
+
+                })
+                .back(20)
+                .turn(Math.toRadians(45))
+                .addTemporalMarker(() -> boxWrist.setPosition(0.71))
+                .waitSeconds(2)
+                .addTemporalMarker(() ->{
+                    latch.setPosition(0.71);
+                })
+                .turn(Math.toRadians(-135))
+                .back(32)
+                .build();
+
+        TrajectorySequence middlePurple = drive.trajectorySequenceBuilder(new Pose2d(0,0,Math.toRadians(0)))
+                .back(40)
+                .addTemporalMarker(() -> boxWrist.setPosition(0.71))
+                .waitSeconds(2)
+                .addTemporalMarker(() ->{
+                    latch.setPosition(0.71);
+                })
+                .turn(Math.toRadians(-90))
+                .back(35)
+                .build();
+
+        TrajectorySequence rightPurple = drive.trajectorySequenceBuilder(new Pose2d(0,0,Math.toRadians(0)))
+                .strafeRight(10)
+                .back(20)
+
+                .build();
+
+        while(!isStarted()){
+            location = bluePropProcessor.getLocation();
+            telemetry.update();
+        }
+        waitForStart();
+        switch(location){
+            case LEFT:
+                drive.followTrajectorySequence(leftPurple);
+                break;
+            case MIDDLE:
+                drive.followTrajectorySequence(middlePurple);
+                break;
+            case RIGHT:
+                drive.followTrajectorySequence(rightPurple);
+                break;
+        }
+
+
+    }
+
+
+
+    public void initHardware(){
+        slide = hardwareMap.get(DcMotor.class, "slide");
+        climb = hardwareMap.get(DcMotor.class, "climb");
+        plane = hardwareMap.get(DcMotor.class, "plane");
+        latch = hardwareMap.get(Servo.class, "latch");
+        boxWrist = hardwareMap.get(Servo.class, "boxWrist");
+        intakeWrist = hardwareMap.get(Servo.class, "intakeWrist");
+        intake = hardwareMap.get(CRServo.class, "intake");
+        belt = hardwareMap.get(CRServo.class, "belt");
+        slide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        slide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        latch.setPosition(1);
+    }
+
+
+
+}
